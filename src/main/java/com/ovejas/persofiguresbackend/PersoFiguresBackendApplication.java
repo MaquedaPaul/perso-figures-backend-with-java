400415debe9ae05faeca0c829449991056054908
Victor Benitez <benitezvictorraul@gmail.com>
2024-02-25 19:35:25 -0300

    installed project fiumba

---
+++ src/main/java/com/ovejas/persofiguresbackend/PersoFiguresBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ovejas.persofiguresbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersoFiguresBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersoFiguresBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ovejas/persofiguresbackend/PersoFiguresBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovejas.persofiguresbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersoFiguresBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
